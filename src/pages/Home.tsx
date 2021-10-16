1ea964ec6f499ee1e71895641d1cd7fad2a27499
Matheus Felipe <matheusfeliperibas@gmail.com>
2021-10-16 00:34:51 -0300

    Removendo todo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,7 +22,7 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(task => task.id !== id))
   }
 
   return (
